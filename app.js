c74265a702f969db8969afa23e740f6063854af3
Oleh Brunets <the.art.of.coding@gmail.com>
2019-11-28 04:11:57 +0200

    fix header injection phase

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ if (CONFIG.allowedOrigins) {
   app.use(function(req, res, next) {
     var origin = req.headers["origin"];
 
-    res.header('Surrogate-Control', 'max-age=86400, stale-while-revalidate=600, stale-if-error=86400');
-
     if (origin) {
       if (CONFIG.allowedOrigins.indexOf('*') > -1) {
         res.setHeader('Access-Control-Allow-Origin', '*');
@@ -39,6 +37,7 @@ if (CONFIG.allowedOrigins) {
 app.disable( 'x-powered-by' );
 app.use(function(req, res, next) {
   res.setHeader('X-Powered-By', 'Iframely');
+  res.setHeader('Surrogate-Control', 'max-age=86400, stale-while-revalidate=600, stale-if-error=86400');
   next();
 });
 
