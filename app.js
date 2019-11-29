0d30ff233eecc88a40a42a25c8d67cc33b669caa
Oleh B <the.art.of.coding@gmail.com>
2019-11-29 04:56:39 +0200

    add cors headers (#4)

--- app.js
+++ app.js
@@ -37,7 +37,13 @@ if (CONFIG.allowedOrigins) {
 app.disable( 'x-powered-by' );
 app.use(function(req, res, next) {
   res.setHeader('X-Powered-By', 'Iframely');
+
   res.setHeader('Surrogate-Control', 'max-age=86400, stale-while-revalidate=600, stale-if-error=86400');
+
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  
   next();
 });
 
